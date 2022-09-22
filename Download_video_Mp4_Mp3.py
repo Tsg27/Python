fa58ee2e8a3d7b4c021a8f19d9dd3dc00320b163
Tiago Soares <85531165+Tsg27@users.noreply.github.com>
2022-09-22 15:43:55 -0300

    Fazer download video do youtube
    O script tem como função fazer download de vidoes do youtube para formato mp4 e mp3 
    ATENÇÃO PARA FUNCIONAR ULTILIZE UMA IDE DE SUA PREFERENCIA Ex: Vscode, COPIE LIKN DO VIDEO E COLE NO INPUT

---
+++ Download_video_Mp4_Mp3.py
@@ -0,0 +1,44 @@
+from tkinter import mainloop
+import youtube_dl
+import os
+os.system('cls')
+
+
+print("\n Escolha opção desejada!!\n ")
+print(" 1-Fazer Download do video MP4")
+print(" 2-Fazer Download video convertido para MP3")
+
+opção = input("\n Digite número da operação (1/2/): ")
+
+
+link = input(' Copie e cole a URL do video: ')
+
+
+if opção == '1':
+
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+
+
+elif opção == '2':
+
+    ydl = youtube_dl.YoutubeDL({
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+})
+
+
+with ydl:
+    result = ydl.extract_info(link, download=True) 
+
+
+if 'entries' in result:
+    video = result['entradas'][0] 
+
+else: 
+    video = result
+
+
