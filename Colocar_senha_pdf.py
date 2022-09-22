86b267bb8312363212d954ee89388b7249023292
Tiago Soares <85531165+Tsg27@users.noreply.github.com>
2022-09-22 15:37:02 -0300

    Colocar_Senha_PDF

--- Colocar_senha_pdf.py
+++ Colocar_senha_pdf.py
@@ -4,15 +4,15 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 
 pdfwriter = PdfFileWriter()
 
-pdf = PdfFileReader('packingslip.pdf')
+pdf = PdfFileReader('packingslip.pdf') # Nome do arquivo antigo
 
 for page_num in range(pdf.numPages):
     pdfwriter.addPage(pdf.getPage(page_num))
 
-passw = 'tiago123'
+passw = 'tiago123' #Senha escolhida
 pdfwriter.encrypt(passw)
 
-with open("modificado.pdf", 'wb') as f:
+with open("modificado.pdf", 'wb') as f: # Nome do arquivo novo
     pdfwriter.write(f)
     f.close()
 
