5bdfda59b4adf479c108ba09fb875e4a23bb6b6d
Tiago Soares <85531165+Tsg27@users.noreply.github.com>
2022-09-22 15:40:38 -0300

    Enviar E-mail para conta gmail
    O script tem como objetivo enviar um mensagem via e-mail no dominio gmail via python

---
+++ EnvariEmail.py
@@ -0,0 +1,20 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from segredos import senha
+
+
+# Configurar email, senha
+EMAIL_ADDRESS = 'tiago.soares@gennesys.com'
+EMAIL_PASSWORD = senha
+
+msg = EmailMessage()
+msg['Subject'] = 'E-mail Teste' # Assunto
+msg['From'] = 'tiago.soares@gennesys.com' # De 
+msg['To'] = 'tiago.soares@gennesys.com' # Destino
+msg.set_content('Olá sou um código Python') #msg
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:    
+    smtp.login(EMAIL_ADDRESS,EMAIL_PASSWORD)
+    smtp.send_message(msg)
