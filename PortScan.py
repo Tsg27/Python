a5fe69bf1d184d931c79c958626927159e8efed1
Tiago Soares <85531165+Tsg27@users.noreply.github.com>
2022-09-22 16:14:54 -0300

    PortScan
    O script tem como função fazer uma scanner na rede conforme IP inserido identificar quais portas estão abertas

---
+++ PortScan.py
@@ -0,0 +1,21 @@
+import os
+import socket
+os.system('cls') 
+
+
+ip = input('Digite o IP: ')
+ports = [20,21,22,23,25,26,80,2222,3306,110
+,443,445,8443,8080,135,139,3306,3395,23395] # portas
+
+for i in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.2)
+    conexao = s.connect_ex((ip,i))
+    if conexao == 0:
+        
+        print('OPEN Port (',i,')')
+       
+    else:
+        exit
+    
+        
